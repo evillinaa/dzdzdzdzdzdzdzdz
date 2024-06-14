67c2bf148c8bce2cf5ab95eb9bd759ec5d9193a6
ДОМ <@DESKTOP-LO335LH>
2024-06-14 10:05:09 +0500

    Добавьте файлы проекта.

---
+++ dzdzdzdzdzdzdzdz/dzdzdzdzdzdzdzdz.cpp
@@ -0,0 +1,30 @@
+﻿#include <iostream>
+
+void func1() 
+{
+    std::cout << "Функция 1 вызвана!" << "\n";
+}
+
+void func2() 
+{
+    std::cout << "Функция 2 вызвана!" << "\n";
+}
+
+void func3() 
+{
+    std::cout << "Функция 3 вызвана!" << "\n";
+}
+
+int main() 
+{
+
+    void (*func_ptrs[3])() = { func1, func2, func3 };
+
+
+    for (int i = 0; i < 3; ++i) 
+    {
+        (*func_ptrs[i])();
+    }
+
+    return 0;
+}
